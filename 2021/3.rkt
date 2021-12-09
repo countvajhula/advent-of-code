a6d30cc586b8e3af57313c2947a13abd438a81da
Siddhartha <sid@countvajhula.com>
2021-12-08 18:12:43 -0800

    solution for day 3, feat. `feedback`

---
+++ 2021/3.rkt
@@ -0,0 +1,62 @@
+#lang racket
+
+(require qi
+         relation
+         "util.rkt")
+
+(define-flow binstring->decimal
+  (~>> (string-append "#b") open-input-string read))
+
+(define-flow bitcounts
+  (~> △ (-< (~> (pass (= 0)) count)
+            (~> (pass (= 1)) count))))
+
+(define-flow mode-bit
+  (~> > (if _ 0 1)))
+
+(define-flow numerify
+  (map (☯ (~>> ->list (map (☯ (~> ->string ->integer)) _))) _))
+
+(define-flow gamma
+  (~>> unzip △ (>< (~> bitcounts mode-bit)) ▽))
+
+(define-flow epsilon
+  (~> gamma △ (>< (if (= 0) 1 0)) ▽))
+
+(define-flow binlist->binstring
+  (~> △ (>< ->string) string-append))
+
+(define-flow part1
+  (~> numerify
+      (-< gamma
+          epsilon)
+      (>< (~> binlist->binstring binstring->decimal))
+      *))
+
+(define (scrubber rate-flo)
+  (☯ (~> numerify
+         (-< 0 rate-flo △)
+         (feedback (while (~> (block 1 2) count (> 1)))
+                   (then (block 1 2))
+                   (-< (~> 1> add1)
+                       (~> (-< (~> (-< 1> 2>)
+                                   (clos (~> (-< (~> (select 2 1) list-ref)
+                                                 (~> (select 3 1) list-ref)) =)))
+                               (block 1 2))
+                           pass
+                           (-< (~> ▽ rate-flo)
+                               _)))))))
+
+(define oxygen (scrubber gamma))
+
+(define co2 (scrubber epsilon))
+
+(define-flow part2
+  (~> (-< oxygen co2)
+      (>< (~> binlist->binstring binstring->decimal))
+      *))
+
+(define input (read-input-file "3.txt"))
+
+(part1 input)
+(part2 input)
