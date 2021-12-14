cf63dfadba69f8b2171dfb4c1c0a83cb1213f53f
Siddhartha <sid@countvajhula.com>
2021-12-14 01:16:10 -0800

    improve day 14 to use a dictionary

--- 2021/14.rkt
+++ 2021/14.rkt
@@ -25,43 +25,41 @@
 (define rules
   (~>> (input) second (map (☯ (~> (-< first third) list)))))
 
-(define-flow (matches-rule? rule pair)
-  (~> (== first _) equal?))
-
-(define-flow (apply-rule rule pair)
-  (~> (when matches-rule? apply-rule*)))
-
-(define-flow (apply-rule* rule pair)
-  (~> (== second _) insert-symbol))
-
-(define-flow (insert-symbol symbol pair)
+(define-flow (apply-rule symbol pair)
   (~> (== ->char (~> ->list △)) (select 2 1 3) ▽ ->string))
-
-(define-flow (pairify polymer)
-  (~>> (infixes 2) ->list △))
 
 (define-flow (trim-last pair*)
   (~> ->list △ (select 1 2) ▽ ->string))
 
-(define-flow (apply-rules rules pair)
+(define ruleshash
+  (make-hash
+   (~>> (rules)
+        (map (☯ (~> (-< first
+                        (~> (-< second first)
+                            apply-rule
+                            trim-last))
+                    cons))))))
+
+(define-flow (pairify polymer)
+  (~>> (infixes 2) ->list △))
+
+(define-flow (apply-rules segment)
   ;; truncates the last char after applying the rule
-  (~> (△ apply-rule) trim-last))
+  (~> (fanout 2) (hash-ref ruleshash _ _)))
 
 (define-flow last-symbol
   (~> ->list last ->string))
 
-(define-flow (polymerize rules polymer)
-  (-< (gen rules)
-      (~> (-< (~> (== (clos apply-rules) pairify)
-                  ><)
-              (~> 2> last-symbol))
-          ▽
-          join)))
+(define-flow (polymerize polymer)
+  (~> (-< (~> pairify (>< apply-rules))
+          last-symbol) ; add back last symbol which was truncated
+      ▽
+      join))
 
-(define (iterate rules polymer N)
-  (~> (1 rules polymer)
+(define (iterate polymer N)
+  (~> (1 polymer)
       (feedback N
-                (then (block 1 2))
+                (then (block 1))
                 (ε (~> 1> displayln)
                    (-< (~> 1> add1)
                        (~> (block 1) polymerize))))))
@@ -83,8 +81,8 @@
 (define-flow drop-zeroes
   (pass (~> second (not (= 0)))))
 
-(define part1
-  (~> (rules template 40)
+(define (reinforce N)
+  (~> (template N)
       iterate
       counts
       drop-zeroes
@@ -93,4 +91,6 @@
       (>< second)
       -))
 
+(define part1 (reinforce 10))
 part1
+;; (define part2 (reinforce 40))
